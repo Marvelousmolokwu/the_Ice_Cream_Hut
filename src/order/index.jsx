3ce361d286bdb0c52f0187de69a16df88651f5c1
Marvelousmolokwu <qmolokwu@gmail.com>
2023-06-10 12:33:29 +0100

    added padding to the mobile view

--- src/order/index.jsx
+++ src/order/index.jsx
@@ -2,7 +2,7 @@ const Order = ({ iceCream }) => {
   return (
     <>
       <section className="h-screen flex  items-center flex-col lg:max-w-[90%] mx-auto    bg-iceCream3  bg-contain bg-no-repeat ">
-        <h2 className="font-bold text-xl sm:text-3xl lg:text-4xl mt-[20vh]">
+        <h2 className="font-bold text-xl sm:text-3xl lg:text-4xl mt-[20vh] px-10">
           Thank you for your Order! :)
         </h2>
         <p className="text-lg sm:text-2xl">
